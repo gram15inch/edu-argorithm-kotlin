ff82c06ef9d392003448e0296bfdd8ac90f615e2
dhkim139 <dhiim139@gmail.com>
2023-09-10 16:51:30 +0900

    삽입 정렬 재구현.

--- CodeStore/src/sort/insertSort.kt
+++ CodeStore/src/sort/insertSort.kt
@@ -13,3 +13,43 @@ fun insertionSort(arr: IntArray) {
         arr[j + 1] = key
     }
 }
+
+fun insertionSort2(arr: IntArray):IntArray {
+    val searchStart = 1
+    val searchEnd = arr.size - searchStart
+
+    var sortedCount = 0
+    do{
+        val select = searchStart + sortedCount
+        var sortedGroupSelect = 0
+        var isNotInsert = true
+        do {
+            if (selectSmallerThanSortedGroupSelect(arr, select, sortedGroupSelect)) {
+                insertAToB(arr, select, sortedGroupSelect)
+                isNotInsert = false
+            }
+        } while (isNotInsert && ++sortedGroupSelect < select)
+
+    } while (++sortedCount < searchEnd)
+
+    return arr
+}
+
+private fun insertAToB(arr: IntArray, put:Int, replace:Int) {
+    val temp = arr[put]
+    moveNextStartToEnd(arr, replace, put)
+    arr[replace] = temp
+}
+
+private fun moveNextStartToEnd(arr: IntArray, start:Int, end:Int){
+    var moveSize = end-start
+    do{
+        val moveEnd = start + moveSize
+        arr[moveEnd] = arr[moveEnd - 1]
+    }
+    while (--moveSize > 0)
+}
+
+private fun selectSmallerThanSortedGroupSelect(array:IntArray, select:Int, target:Int):Boolean{
+    return array[select] < array[target]
+}
--- src/test/kotlin/SortTest.kt
+++ src/test/kotlin/SortTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import sort.bubbleSort2
+import sort.insertionSort2
 
 class SortTest {
 
@@ -23,5 +24,26 @@ class SortTest {
 
         assertEquals(expect, actual.toList())
     }
+
+    @Test
+    fun insertWhenWorstTest(){
+
+        val input = List(10){ 9-it}
+        val expect = List(10){it}
+        val actual = insertionSort2(input.toIntArray())
+
+        assertEquals(expect, actual.toList())
+    }
+
+    @Test
+    fun insertTest(){
+
+        val input = listOf(9,1,8,5,6,4,3,7,2,0)
+        val expect = List(10){it}
+        val actual = insertionSort2(input.toIntArray())
+
+        assertEquals(expect, actual.toList())
+    }
+
 
 }
