ef12b45b88924998e21ac158828817ebd8861905
dhkim139 <dhiim139@gmail.com>
2023-09-10 13:55:46 +0900

    버블정렬 테스트 케이스 추가.

--- src/test/kotlin/SortTest.kt
+++ src/test/kotlin/SortTest.kt
@@ -1,14 +1,23 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import sort.bubbleSort
 import sort.bubbleSort2
 
 class SortTest {
 
     @Test
-    fun bubbleTest(){
+    fun bubbleWhenWorstTest(){
 
         val input = List(10){ 9-it}
+        val expect = List(10){it}
+        val actual = bubbleSort2(input.toIntArray())
+
+        assertEquals(expect, actual.toList())
+    }
+
+    @Test
+    fun bubbleTest(){
+
+        val input = listOf(9,1,8,5,6,4,3,7,2,0)
         val expect = List(10){it}
         val actual = bubbleSort2(input.toIntArray())
 
