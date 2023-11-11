ec78a397e9e4e934711a8fe5e92cbef5279c9bfa
dhkim139 <dhkim139@gmail.com>
2023-11-11 20:00:30 +0900

    24060 진행중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,60 +1,61 @@
-
-import java.io.BufferedWriter
-
+import java.util.StringTokenizer
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-    val counts = br.readLine().toInt()
-    val leftArray = Array<Int>(counts+1){-1}
-    var inTop = 0
-    var leftIdx = -1
-    val peopleInLine = br.readLine().toString()
-    val peopleArray = peopleInLine.split(" ")
-    var isNotWrite = true
-    for(it in peopleArray) {
-        val peopleNum = it.toInt()
-        when{
-            peopleNum == inTop + 1 -> inTop++
-            leftIdx == -1 -> leftArray[++leftIdx] = peopleNum
+    val st1= StringTokenizer(br.readLine())
+    val arraySize = st1.nextToken().toInt()
+    val saveTimes = st1.nextToken().toInt()
 
-            peopleNum < leftArray[leftIdx] -> { leftArray[++leftIdx] = peopleNum }
+    val st2 = StringTokenizer(br.readLine())
+    val array =IntArray(arraySize){st2.nextToken().toInt()}
 
-            peopleNum > leftArray[leftIdx] && inTop+1 == leftArray[leftIdx] -> {
-                inTop++
-                leftIdx--
-                while (leftIdx !=-1 && inTop+1 == leftArray[leftIdx]){
-                    inTop++
-                    leftIdx--
-                }
-                if(peopleNum == inTop+1)
-                    inTop++
-                else
-                    leftArray[++leftIdx] = peopleNum
-            }
-            else -> {
-                bw.write("Sad")
-                isNotWrite = false
-                break
-            }
-        }
-    }
-    if(isNotWrite)
-        bw.write("Nice")
+    val sortedArray = mergeSort(array,0,array.size-1,saveTimes)
+    bw.write(sortedArray.toList().toString())
+    bw.write("\n"+last)
     bw.flush()
 }
 
+var count =0
+var last = 0
+fun mergeSort(array: IntArray, leftIdx:Int, rightIdx:Int, saveTime:Int): IntArray {
+    if(leftIdx == rightIdx)
+        return IntArray(1){array[leftIdx]}
+    val midIdx :Int = (leftIdx + rightIdx)/2
 
-fun BufferedWriter.printLines(lines:List<String>){
-    lines.forEach {
-        this.write("$it\n")
+    val leftArray = mergeSort(array, leftIdx, midIdx, saveTime)
+    val rightArray = mergeSort(array,midIdx+1, rightIdx, saveTime)
+
+    return merge(leftArray,rightArray, saveTime)
+}
+fun merge(leftArr:IntArray, rightArr: IntArray, saveTime: Int): IntArray {
+    val mergeArr = IntArray(leftArr.size + rightArr.size)
+    var lIdx = 0
+    var rIdx = 0
+
+    while(lIdx+rIdx <= mergeArr.size-1){
+        val result:Int
+        if(lIdx < leftArr.size && rIdx< rightArr.size) {
+            if (leftArr[lIdx] < rightArr[rIdx]) {
+                mergeArr[lIdx + rIdx] = leftArr[lIdx]
+                result = leftArr[lIdx++]
+            }
+            else {
+                mergeArr[lIdx + rIdx] = rightArr[rIdx++]
+                result = rightArr[rIdx++]
+            }
+        }else if(lIdx < leftArr.size){
+            mergeArr[lIdx + rIdx] = leftArr[lIdx]
+            result = leftArr[lIdx++]
+        }else {
+            mergeArr[lIdx + rIdx] = rightArr[rIdx]
+            result = leftArr[lIdx++]
+        }
+
+        if(++count == saveTime)
+            last = result
     }
-    this.flush()
-}
 
-fun BufferedWriter.printLines(line:String){
-    this.write(line)
-    this.flush()
+    return mergeArr
 }
-
