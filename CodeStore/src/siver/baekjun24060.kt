7e4d998bd41e6e1166c13e707b15a878d7e54ceb
dhkim139 <dhkim139@gmail.com>
2023-12-23 13:57:37 +0900

    24060 완료

--- CodeStore/src/siver/baekjun24060.kt
+++ CodeStore/src/siver/baekjun24060.kt
@@ -1,50 +1,75 @@
 package siver
 
+import java.util.*
+
+@Suppress("unused")
 fun baekjun24060() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
 
+    val st1= StringTokenizer(br.readLine())
+    val arraySize = st1.nextToken().toInt()
+    val saveTimes = st1.nextToken().toInt()
+    val ms = NumInMergeSort(arraySize, saveTimes)
+    val st2 = StringTokenizer(br.readLine())
+    val array =IntArray(arraySize){st2.nextToken().toInt()}
+
+    ms.merge_sort(array,0,array.size-1)
+    bw.write("${ms.num}")
+    bw.flush()
 }
 
-var count =0
-var last = 0
+class NumInMergeSort(arraySize :Int, val saveTimes:Int){
+    var count = 0
+    var num = -1
+    var isNotFInd = true
+    var tmpArr = IntArray(arraySize)
 
-private fun mergeSort(array: IntArray, leftIdx:Int, rightIdx:Int, saveTime:Int): IntArray {
-    if(leftIdx == rightIdx)
-        return IntArray(1){array[leftIdx]}
-    val midIdx :Int = (leftIdx + rightIdx)/2
-
-    val leftArray = mergeSort(array, leftIdx, midIdx, saveTime)
-    val rightArray = mergeSort(array,midIdx+1, rightIdx, saveTime)
-
-    return merge(leftArray,rightArray, saveTime)
-}
-
-private fun merge(leftArr:IntArray, rightArr: IntArray, saveTime: Int): IntArray {
-    val mergeArr = IntArray(leftArr.size + rightArr.size)
-    var lIdx = 0
-    var rIdx = 0
-
-    while(lIdx+rIdx <= mergeArr.size-1){
-        val result:Int
-        if(lIdx < leftArr.size && rIdx< rightArr.size) {
-            if (leftArr[lIdx] < rightArr[rIdx]) {
-                mergeArr[lIdx + rIdx] = leftArr[lIdx]
-                result = leftArr[lIdx++]
-            }
-            else {
-                mergeArr[lIdx + rIdx] = rightArr[rIdx++]
-                result = rightArr[rIdx++]
-            }
-        }else if(lIdx < leftArr.size){
-            mergeArr[lIdx + rIdx] = leftArr[lIdx]
-            result = leftArr[lIdx++]
-        }else {
-            mergeArr[lIdx + rIdx] = rightArr[rIdx]
-            result = leftArr[lIdx++]
+    fun merge_sort(arr:IntArray, start:Int, end:Int){
+        if(start < end && isNotFInd){
+            val mid:Int = (start + end) / 2
+            merge_sort(arr, start, mid)
+            if(!isNotFInd)
+                return
+            merge_sort(arr, mid+1, end)
+            if(!isNotFInd)
+                return
+            merge(arr, start, mid, end)
         }
-
-        if(++count == saveTime)
-            last = result
     }
 
-    return mergeArr
+    fun merge(arr: IntArray, start: Int, mid: Int, end: Int) {
+        if(isNotFInd){
+            var lStart = start
+            var rStart = mid + 1
+            var tmp = 0
+
+            while (lStart <= mid && rStart <= end) {
+                if (arr[lStart] <= arr[rStart])
+                    tmpArr[tmp++] = arr[lStart++]
+                else
+                    tmpArr[tmp++] = arr[rStart++]
+            }
+
+            while (lStart < mid + 1) {
+                tmpArr[tmp++] = arr[lStart++]
+            }
+
+            while (rStart <= end) {
+                tmpArr[tmp++] = arr[rStart++]
+            }
+
+            var mStart = start
+            var mTmp = 0
+            while (mStart <= end) {
+                count++
+                if (count == saveTimes) {
+                    num = tmpArr[mTmp]
+                    //  isNotFInd = false
+                    //  return
+                }
+                arr[mStart++] = tmpArr[mTmp++]
+            }
+        }
+    }
 }
