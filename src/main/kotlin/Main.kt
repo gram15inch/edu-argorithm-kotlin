2aa82d35033fc03b9f5a4f87586421ba331939b1
dhkim139 <dhkim139@gmail.com>
2024-01-06 18:25:56 +0900

    1018 진행중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,109 @@
-import java.util.StringTokenizer
-import kotlin.math.pow
+import java.util.*
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-    val st1= StringTokenizer(br.readLine())
-    val n = st1.nextToken().toInt()
-    val barLength = (3.0).pow(n)
-    val bar = Array(barLength.toInt()){"-"}
-    kantorSet(bar,0,barLength.toInt()-1)
 
-    bw.write("${bar.toList()}")
+    val st1= StringTokenizer(br.readLine())
+    val M = st1.nextToken().toInt()
+    val N = st1.nextToken().toInt()
+    val arr = Array(M){br.readLine()}
+    val max =2
+    val cArr = Array((M-(max-1))*(N-(max-1))){ arrayOf(0,0,0)}
+    for(m in 0..M-max)
+        for(n in 0..N-max) {
+            count=0
+            countW=0
+            countB=0
+            val startM = m
+            val startN = n
+            for (row in 0..< max) {
+                for (col in 0..< max) {
+                    val color = arr[startM+row][startN+col]
+                    if (row == 0 && col == 0) {
+                        when(color){
+                            'W'-> {
+                                isFirstW = true
+                            }
+                            'B'-> {
+                                isFirstW = false
+                            }
+                            else-> throw Exception("색오류")
+                        }
+                    }
+                    else {
+                        if(isFirstW){
+                            countUpWhenNonChess(true,row, col, color)
+                            countW = count
+                            count=1
+                            countUpWhenNonChess(false,row, col, color)
+                            countB = count
+
+                        }else{
+                            countUpWhenNonChess(false,row, col, color)
+                            countB = count
+                            count=1
+                            countUpWhenNonChess(true,row, col, color)
+                            countW = count
+                        }
+                        if(countW<=countB)
+                            count= countW
+                        else
+                            count= countB
+                    }
+                }
+            }
+
+            cArr[cArrCount][0] = startM
+            cArr[cArrCount][1] = startN
+            cArr[cArrCount++][2] = count
+        }
+   // bw.write(cArr.min().toString())
     bw.flush()
 }
-
-//4779
-fun kantorSet(arr:Array<String>,start:Int, end:Int){
-    val lMid:Int = ((end+1)/3) -1
-    val rMid:Int = (((end+1)/3)*2) -1
-    if(end-start >= 2 && lMid < end && rMid>start){
-        kantorSet(arr,start, lMid)
-        kantorSet(arr, rMid, end)
-        deleteBar(arr,lMid+1, rMid-1)
+var isFirstW = true
+var count = 0
+var countW = 0
+var countB = 0
+var cArrCount = 0
+private fun countUpWhenNonChess(isFirstW:Boolean ,row:Int, col:Int, color:Char){
+    if(isFirstW) {
+        if ((row % 2) == 0) {
+            if(col % 2 ==0) {
+                if (color != 'W')
+                    count++
+            }else{
+                if (color != 'B')
+                    count++
+            }
+        } else
+            if(col % 2 ==0) {
+                if (color != 'B')
+                    count++
+            }else{
+                if (color != 'W')
+                    count++
+            }
+    }else{
+        if ((row % 2) ==0) {
+            if(col % 2 ==0) {
+                if (color != 'B')
+                    count++
+            }else{
+                if (color != 'W')
+                    count++
+            }
+        } else
+        {
+            if(col % 2 ==0) {
+                if (color != 'W')
+                    count++
+            }else{
+                if (color != 'B')
+                    count++
+            }
+        }
     }
 }
 
-fun deleteBar(arr:Array<String>, start: Int, end:Int) {
-    (start..end).forEach {
-        arr[it] =""
-    }
-}
