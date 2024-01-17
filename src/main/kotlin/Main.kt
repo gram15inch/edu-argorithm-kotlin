b424866a84c1a69c8cdd823fafd7f34240bfb1c4
dhkim139 <dhkim139@gmail.com>
2024-01-17 20:40:06 +0900

    18870 진행중2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,61 @@
 import java.util.StringTokenizer
 
-@Suppress("unused")
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
     val n= br.readLine().toInt()
-    size =n
     val st =  StringTokenizer(br.readLine())
-    val arr = Array(n){st.nextToken().toInt() }
+    val arr1 = Array(n){st.nextToken().toInt() }
+    val arr2 = Array(arr1.size){0}
+    var list1 = mutableListOf<Int>()
+    var arr3 = IntArray(0)
+    for(num1 in 0..< arr1.size) {
+        list1.clear()
+        arr3 = IntArray(0)
+        for (num2 in 0..<arr1.size) {
+            val a = arr1[num1]
+            val b = arr1[num2]
+            if (a > b && !isContains(arr3, b)) {
+                arr2[num1]++
+                list1.add(b)
+                arr3 = list1.toIntArray()
+                insertionSort(arr3)
+                list1 = arr3.toMutableList()
+            }
+        }
+    }
 
-    mergeSort(arr,0, arr.size-1)
-
-    for(num in arr){
+    for(num in arr2){
         bw.write("$num ")
     }
     bw.flush()
 }
 
-private fun mergeSort(arr:Array<Int>, start:Int, end:Int){
-    if(start < end){
-        val mid:Int = (start + end) / 2
-        mergeSort(arr, start, mid)
-        mergeSort(arr, mid+1, end)
-        merge(arr, start, mid, end)
+private fun isContains(arr:IntArray,num:Int):Boolean{
+    arr.forEach {
+        if(it<num)
+            return false
+        if(it==num)
+            return true
     }
-}
-private var size = 0
-private val tmpArr by lazy { Array(size) { 0 } }
-private fun merge(arr: Array<Int>, start: Int, mid: Int, end: Int) {
-    var lStart = start
-    var rStart = mid+1
-    var tmp = 0
-
-    while (lStart <= mid && rStart <= end){
-        if(arr[lStart] < arr[rStart])
-            tmpArr[tmp++] = arr[lStart++]
-        else
-            tmpArr[tmp++] = arr[rStart++]
-    }
-
-    while (lStart<mid+1){
-        tmpArr[tmp++] = arr[lStart++]
-    }
-
-    while (rStart<=end){
-        tmpArr[tmp++] = arr[rStart++]
-    }
-
-    var mStart = start
-    var mTmp = 0
-    while (mStart <= end){
-        arr[mStart++] = tmpArr[mTmp++]
-    }
+    return false
 }
 
+
+fun insertionSort(arr: IntArray) {
+    val n = arr.size
+
+    for (i in 1 until n) {
+        val key = arr[i]
+        var j = i - 1
+
+        // 이미 정렬된 부분에서 key보다 큰 원소를 찾아 적절한 위치에 삽입
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+
+        arr[j + 1] = key
+    }
+}
