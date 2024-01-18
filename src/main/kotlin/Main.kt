6636a72cc942fccb2786c48066eb90842c3d1cbe
dhkim139 <dhkim139@gmail.com>
2024-01-18 20:44:41 +0900

    18870 진행중3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import java.util.StringTokenizer
 
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
@@ -7,55 +8,26 @@ fun main() {
     val n= br.readLine().toInt()
     val st =  StringTokenizer(br.readLine())
     val arr1 = Array(n){st.nextToken().toInt() }
-    val arr2 = Array(arr1.size){0}
-    var list1 = mutableListOf<Int>()
-    var arr3 = IntArray(0)
-    for(num1 in 0..< arr1.size) {
-        list1.clear()
-        arr3 = IntArray(0)
-        for (num2 in 0..<arr1.size) {
-            val a = arr1[num1]
-            val b = arr1[num2]
-            if (a > b && !isContains(arr3, b)) {
-                arr2[num1]++
-                list1.add(b)
-                arr3 = list1.toIntArray()
-                insertionSort(arr3)
-                list1 = arr3.toMutableList()
-            }
-        }
-    }
+    val arr2 = arr1.toSet().toIntArray()
+    arr2.sortDescending()
 
-    for(num in arr2){
+    for(num1 in arr1) {
+       val num = parseNum(arr2, num1)
         bw.write("$num ")
     }
+
     bw.flush()
 }
 
-private fun isContains(arr:IntArray,num:Int):Boolean{
-    arr.forEach {
-        if(it<num)
-            return false
-        if(it==num)
-            return true
-    }
-    return false
-}
-
-
-fun insertionSort(arr: IntArray) {
-    val n = arr.size
-
-    for (i in 1 until n) {
-        val key = arr[i]
-        var j = i - 1
-
-        // 이미 정렬된 부분에서 key보다 큰 원소를 찾아 적절한 위치에 삽입
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j]
-            j--
+private fun parseNum(arr2:IntArray, num:Int):Int{
+    arr2.forEachIndexed { index, it ->
+        if(num >= it) {
+            return arr2.size - (index + 1)
         }
-
-        arr[j + 1] = key
     }
+    return 0
 }
+
+
+
+
