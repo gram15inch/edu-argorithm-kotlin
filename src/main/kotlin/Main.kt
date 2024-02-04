ce1312d645ef2253b5d9d6b98e1b92eaf9b93c80
dhkim139 <dhkim139@gmail.com>
2024-02-04 22:32:45 +0900

    13241 진행중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,67 @@
-import java.util.*
+import java.io.StreamTokenizer
+import java.util.HashSet
+import java.util.StringTokenizer
 import kotlin.collections.HashMap
-
+import kotlin.math.sign
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-    val n= br.readLine().toInt()
-    val hash = HashMap<String,Boolean>()
-
-    repeat(n) {
-        val st = StringTokenizer(br.readLine())
-        val name = st.nextToken()
-        val status = st.nextToken()
-
-        hash[name]=when(status){
-            "enter" -> true
-            else -> false
+    StreamTokenizer(br).apply {
+        fun getInt() : Int{
+            nextToken()
+            return nval.toInt()
         }
-    }
 
-    hash.asSequence().filter{it.value}.map{it.key}.sortedByDescending { it }.forEach {
-        bw.write("${it}\n")
-    }
+        fun getString():String{
+            nextToken()
+            return sval.toString()
+        }
 
+        var ma =0
+        var mb =0
+        var mal =0L
+        var mbl =0L
+        var isLong =false
+        getString().apply {
+            isLong = this.length > 8
+            ma = this.toInt()
+            mal = this.toLong()
+        }
+        getString().apply {
+            isLong = this.length > 8
+            mb = this.toInt()
+            mbl = this.toLong()
+        }
+
+        fun<T:Number> test1(pa:T,pb:T) {
+            var a = with(pa) { if(this is Int) this.toInt() else this.toLong() }
+            var b = with(pb) { if(this is Int) this.toInt() else this.toLong() }
+            var gcd :Int
+            val ab :Long
+
+            when{
+                a==1 -> bw.write("$b\n")
+                b==1 -> bw.write("$a\n")
+                else->{
+                    ab = (a * b).toLong()
+                    do{
+                        gcd = a%b
+                        if(gcd != 0) {
+                            a = b
+                            b = gcd
+                        }
+                        else
+                            break
+                    }
+                    while (true)
+                    bw.write("${ab/b}\n")
+                }
+            }
+        }
+
+    }
     bw.flush()
+
 }
-
-
-
-
-
