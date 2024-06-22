ff00bcc11b892c6d3e2d1f9912668d8890c26515
dhkim139 <dhkim139@gmail.com>
2024-06-22 16:52:06 +0900

    9663 진행중2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,48 +12,29 @@ fun main() {
         }
 
         val n = getInt()
-        val chess = Array(n) {
-            BooleanArray(n){true}
-        }
-        var count =0
+        val chess = Array(n) {-1}
 
-        fun check(x:Int,y:Int,code:Int) {
-            if (x > n - 1 || y > n - 1 || x < 0 || y < 0)
-                return
-            if (chess[y][x])
-                chess[y][x] = false
-            when (code) {
-                0 -> check(x + 1, y, 0)
-                1 -> check(x - 1, y, 1)
-                2 -> check(x, y + 1, 2)
-                3 -> check(x, y - 1, 3)
-                4 -> check(x + 1, y + 1, 4)
-                5 -> check(x + 1, y - 1, 5)
-                6 -> check(x - 1, y - 1, 6)
-                7 -> check(x - 1, y + 1, 7)
-            }
-        }
-    //todo 다시돌아올떄 그전에 변경사항 되돌릴지?
-        fun dfs(k:Int,x:Int,y:Int){
-            if(k>n-1)
-                return
-            if(k==n) {
+        var count = 0
+        var col = 0
+
+        fun bt(stack:Int){
+            if(col>n-1) {
                 count++
                 return
             }
-            for (code in 0..7)
-                check(x,y,code)
 
-            for(x in 0..<n)
-                for(y in 0..<n){
-                    if(chess[x][y]) {
-                        chess[x][y] = false
-                        dfs(k+1,x,y)
-                        chess[x][y] = true
-                    }
-                }
+            if(stack>n-1) {
+                col++
+                bt(0)
+                return
+            }
+            //todo 열우선 초기화 해보기
+            chess[stack] = col
+            bt(stack+1)
+            col = 0
+
         }
-        dfs(0,0,0)
+        bt(0)
         bw.write(count.toString())
 
     }
