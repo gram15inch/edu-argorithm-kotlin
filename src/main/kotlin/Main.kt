eb15ee3ca8925364832827804aaa9a8de465a006
dhkim139 <dhkim139@gmail.com>
2024-07-20 13:32:29 +0900

    10845 진행중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,27 +5,60 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
+
     StreamTokenizer(br).apply {
         fun getInt(): Int {
             nextToken()
             return nval.toInt()
         }
 
+        fun getString(): String {
+            nextToken()
+            return sval.toString()
+        }
 
         val n = getInt()
-        val hash1 = HashSet<Int>(n)
+        val arr = IntArray(n)
+        var front = -1
+        var back = 0
         repeat(n){
-            hash1.add(getInt())
+            when(getString()){
+                "push"->{
+                    val nn = getInt()
+                    if(front<0){
+                        front++
+                        arr[0] = nn
+                    }
+                    front++
+                    arr[front] = nn
+                }
+                "pop"->{
+                    if(front < back && front > 0) {
+                        bw.write("${arr[front]}\n")
+                        front++
+                    } else {
+                        bw.write("-1\n")
+                    }
+                }
+                "size"->{
+                    bw.write("${back-front+1}\n")
+                }
+                "empty"->{
+                    bw.write("${if(front==-1) 1 else 0}\n")
+                }
+                "front"->{
+                    bw.write("${if(front<0) -1 else arr[front] }\n")
+                }
+                "back"->{
+                    bw.write("${if(front<0) -1 else arr[front] }\n")
+                }
+            }
         }
 
-        val m = getInt()
-        repeat(m) {
-            val num = getInt()
-            bw.write(if (hash1.contains(num)) "1\n" else "0\n")
-        }
     }
-
     bw.flush()
+
+
 
 }
 
