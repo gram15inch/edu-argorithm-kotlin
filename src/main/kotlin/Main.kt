eeccf30e1e773794facfb9c43a7d42d45e091b65
dhkim139 <dhkim139@gmail.com>
2024-08-03 06:46:12 +0900

    // 7568 진행중

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,63 +2,29 @@ import java.io.StreamTokenizer
 
 
 fun main() {
+
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
 
     StreamTokenizer(br).apply {
+
         fun getInt(): Int {
             nextToken()
             return nval.toInt()
         }
 
-        fun getString(): String {
-            nextToken()
-            return sval.toString()
-        }
-
+        data class Body(val key:Int, val mom:Int)
         val n = getInt()
-        val arr = IntArray(n)
-        var front = -1
-        var back = 0
+        val list1 = mutableListOf<Body>()
         repeat(n){
-            when(getString()){
-                "push"->{
-                    val nn = getInt()
-                    if(front<0){
-                        front++
-                        arr[0] = nn
-                    }
-                    front++
-                    arr[front] = nn
-                }
-                "pop"->{
-                    if(front < back && front > 0) {
-                        bw.write("${arr[front]}\n")
-                        front++
-                    } else {
-                        bw.write("-1\n")
-                    }
-                }
-                "size"->{
-                    bw.write("${back-front+1}\n")
-                }
-                "empty"->{
-                    bw.write("${if(front==-1) 1 else 0}\n")
-                }
-                "front"->{
-                    bw.write("${if(front<0) -1 else arr[front] }\n")
-                }
-                "back"->{
-                    bw.write("${if(front<0) -1 else arr[front] }\n")
-                }
-            }
+            list1.add(Body(getInt(), getInt()))
         }
 
+        // 7568 진행중
     }
+
     bw.flush()
-
-
 
 }
 
