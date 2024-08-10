3f5ac0dc9347dfa75c31c3eb00c35ae6bb8b6342
dhkim139 <dhkim139@gmail.com>
2024-08-10 23:50:38 +0900

    1966 진행중2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,53 @@
 import java.io.StreamTokenizer
+import java.util.LinkedList
+import java.util.Queue
 
 
 fun main() {
-
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
-
     StreamTokenizer(br).apply {
-
         fun getInt(): Int {
             nextToken()
             return nval.toInt()
         }
 
-        data class Body(val key:Int, val mom:Int)
-        val n = getInt()
-        val list1 = mutableListOf<Body>()
-        repeat(n){
-            list1.add(Body(getInt(), getInt()))
-        }
 
-        // 7568 진행중
+        val n = getInt()
+        repeat(n){
+            val size = getInt()
+            val searchIdx = getInt()
+            val que:Queue<Pair<Int,Int>> = LinkedList<Pair<Int,Int>>()
+
+            var max = 9
+            repeat(size){
+                que.add(Pair(it,getInt()))
+            }
+
+            var isWrite = false
+            var delete = 0
+            while (!isWrite){
+
+                if(max == que.peek().second){
+                    delete++
+                    que.poll().apply {
+                        if(searchIdx==this.first){
+                            isWrite=true
+                            bw.write("$delete\n")
+                        }else{
+                            que.add(this)
+                        }
+                    }
+                }
+            }
+
+
+        }
     }
 
     bw.flush()
 
 }
-
 
 
