5cf6cb7bda00c41ff42fb452dd2ad88df5029658
gram15inch <94951889+gram15inch@users.noreply.github.com>
2024-09-05 16:12:57 +0900

    11659 완료

---
+++ CodeStore/src/siver/baekjun11659.kt
@@ -0,0 +1,38 @@
+package siver
+
+import java.io.StreamTokenizer
+
+private fun baekjun11659() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    StreamTokenizer(br).apply {
+        fun getInt(): Int {
+            nextToken()
+            return nval.toInt()
+        }
+
+        val n = getInt() // ~10만
+        val m = getInt() // ~10만
+        val arr= IntArray(n+1) // 1000
+        val nuArr = IntArray(n+1)
+        for(idx in 1 .. n)
+            arr[idx]=getInt()
+
+        nuArr[1] = arr[1]
+        for(idx in 1 .. n)
+            nuArr[idx]= nuArr[idx-1] + arr[idx]
+        repeat(m){
+            val i = getInt() // ~n
+            val j = getInt() // ~n
+            val r = if(i!=1)
+                nuArr[j] - nuArr[i-1]
+            else
+                nuArr[j]
+            bw.write("$r\n")
+        }
+
+    }
+
+    bw.flush()
+}
