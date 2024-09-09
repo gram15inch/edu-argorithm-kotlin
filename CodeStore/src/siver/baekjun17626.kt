cec6885f265f58997ba8ecf5a711bf205201af94
gram15inch <94951889+gram15inch@users.noreply.github.com>
2024-09-09 18:25:38 +0900

    17626 완료

---
+++ CodeStore/src/siver/baekjun17626.kt
@@ -0,0 +1,42 @@
+package siver
+
+import java.io.StreamTokenizer
+import kotlin.math.sqrt
+
+private fun baekjun17626 (){
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    StreamTokenizer(br).apply {
+        fun getInt(): Int {
+            nextToken()
+            return nval.toInt()
+        }
+
+        val n = getInt() // ~50000
+
+        val dp = IntArray(n + 4)
+
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 3
+
+        var min = 500001
+        for (num in 4..n) {
+            min = 500001
+            for (minSqrt in 1..sqrt(num.toFloat()).toInt()) {
+                (minSqrt * minSqrt).apply {
+                    val before = dp[this] + dp[num-this]
+                    if (min > before)
+                        min = before
+                }
+            }
+            dp[num] = min
+        }
+
+        bw.write("${dp[n]}\n")
+    }
+
+    bw.flush()
+}
